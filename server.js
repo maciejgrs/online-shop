b34821b0256f0032881eb9502e5caae88ec28a83
Maciej Grs <maciejgrs@gmail.com>
2021-02-14 09:26:02 +0100

    html pages addes, path module

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
@@ -13,7 +14,7 @@ app.use('/admin', adminRoutes)
 app.use(shopRoutes) 
 
  app.use((req, res) => {
-    res.status(404).send('<h1>Page not found 404</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
  })
 
 app.listen(3000);
